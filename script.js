b1b3f71035326a3124743fe06daa3b7d35ac0d75
ajringst <aj.ringstad@gmail.com>
2022-07-07 13:59:09 -0600

    make conditionals for playRound

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-let playerSelection = "rock";
+let playerSelection = " ";
 let computerSelection = "rock";
 
 
 function getPlayerSelection(){
-    playerSelection = document.getElementById("choice").value;
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = document.getElementById("choice").value.toLowerCase();
+    return playerSelection;
+    }
     
-    if (playerSelection != "rock" || "paper" || "scissors") {
-        document.getElementById("outcome").innerHTML = "Please enter a valid choice!";
-    }
-    }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = getPlayerSelection();
-    if (playerSelection === computerSelection){
-        document.getElementById("outcome").innerHTML = "You Tied!";
+    console.log(playerSelection);
+    if (playerSelection === "rock" || "paper" || "scissors"){
+        console.log(playerSelection);
+             
+        if (playerSelection === computerSelection){
+            document.getElementById("outcome").innerHTML = "You Tied!";
+            console.log(playerSelection);
+            }
+
+        else if ((playerSelection === "rock" && computerSelection === "scissors") || 
+                (playerSelection === "scissors" && computerSelection === "paper") || 
+                (playerSelection === "paper" && computerSelection === "rock" )) {
+                document.getElementById("outcome").innerHTML = "You Won!";
+                console.log(playerSelection)
+                }
+
+        else if ((playerSelection === "rock" && computerSelection === "paper") || 
+                (playerSelection === "scissors" && computerSelection === "rock") || 
+                (playerSelection === "paper" && computerSelection === "scissors")) {
+                document.getElementById("outcome").innerHTML = "You Lost!";
+                console.log(playerSelection)
+                }
+            }        
+
+    else {
+        document.getElementById("outcome").innerHTML = "Please enter a valid choice!";
+        console.log("Something has gone incredibly wrong!!!!");
     }
-    
 }
