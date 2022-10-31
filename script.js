4e046f6f07e53a591d91d275a2682ea5771bbb8f
ajringst <aj.ringstad@gmail.com>
2022-10-31 14:55:19 -0600

    add computerSelection function to make choice random

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//let playerSelection = " ";
-//let computerSelection = " ";
+let playerSelection = " ";
+let computerSelection = " ";
 let playerScore = 0;
 let computerScore = 0;
 
@@ -20,12 +20,27 @@ let buttons = document.querySelectorAll("button");
 //buttons.forEach(button => button.addEventListener("click", getPlayerSelection));
 buttons.forEach(button => button.addEventListener("click", e => playRound(e.target.id), false));
 
-
+function getComputerSelection(){
+    let randomizer = Math.random();
+    console.log(randomizer);
+    if (randomizer <= 0.3333){
+        return "rock";
+    }
+    else if (randomizer > 0.3333 && randomizer < 0.6667)
+        return "paper";
+    else if (randomizer >= 0.6667) {
+        return "scissors";
+    }
+    else {
+        console.log("Error");
+        alert("You've ran into an error");
+    }
+}
     
 
 function playRound(playerSelection) {
-    computerSelection = "rock";
-    console.log(playerSelection);
+    computerSelection = getComputerSelection();
+    console.log(computerSelection);
     if (playerSelection === computerSelection){
             document.getElementById("outcome").innerHTML = "You Tied!";
             console.log(playerSelection);
